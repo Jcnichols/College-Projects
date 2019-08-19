e00836fe4734cfd692eb3a2474a1b128b2ba4e0a
john nichols <53110467+Jcnichols@users.noreply.github.com>
2019-08-19 11:50:18 -0500

    Add files via upload
    for loops

---
+++ test project/src/junk.java
@@ -0,0 +1,12 @@
+
+public class junk {
+
+	public static void main(String[] args) {
+		int[] array = new int[]{1,2,3};
+		for(int i = 0; i < array.length; i++) {
+			for(int j = 0; j < array.length; j++) {
+				System.out.println(array[i] + "," + array[j]);
+			}
+		}
+	}
+}
---
+++ test project/src/junky.java
@@ -0,0 +1,11 @@
+
+public class junky {
+	  public static void main (String[] args){ 
+		  int[] narray = new int[] {4,5,6};
+		  for(int i = 0; i < narray.length; i++) { 
+			  for(int j = 0; j < narray.length; j++) {;
+					  System.out.println(narray[i] + " , " + narray[j]);
+			  }
+	  			}
+	  		}
+	  }
